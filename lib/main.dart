b2e1824d77e1b6ec339a0a265e574bc28f605060
Japunat <148606349+Japunat@users.noreply.github.com>
2023-12-20 03:48:27 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:gdsc/first.dart';
+
+
+void main() {
+
+  runApp(MaterialApp(
+    home: const First(),
+    theme: ThemeData(useMaterial3: true),
+  ));
+}
